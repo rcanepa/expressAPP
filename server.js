d52b5d8bd693232ecb1000eaef6c604d01af3bc3
rcanepa <rcanepag@gmail.com>
2014-10-28 21:58:40 -0300

    Testing using the route function

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ var names = [];
 
 // my route functions
 
+app.route('/')
+	.all(log, function(req, res, next){
+		console.log('all');
+		next();
+	})
+	.get(function(req, res){
+		console.log('get');
+		res.render('index.jade', {
+			title: 'Hello Express & Jade',
+			names: names
+		});
+	})
+	.post(function(req, res){
+		console.log('post');
+		names.push(req.body.name);
+		res.redirect('/');
+	});
+
+
+/*
 app.all('/', log, function(req, res, next){
 	console.log('all');
 	next();
@@ -41,6 +61,20 @@ app.post('/', function(req, res){
 	names.push(req.body.name);
 	res.redirect('/');
 });
+*/
+
+app.get('/route', function(req, res, next){
+	res.send('This is another route!');
+	next();
+});
+
+// route parameters
+app.get('/names/:name', function(req, res){
+	res.send('Your name is ' + req.params.name);
+});
+
+// built-in middleware
+app.use(express.static('./public'));
 
 app.listen(3000, function(){
 	console.log('listening on port 3000');
