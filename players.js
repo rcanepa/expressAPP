9fc6a28d6e09e9380dbfe98948ff97beb0e6476e
rcanepa <rcanepag@gmail.com>
2014-10-28 23:49:29 -0300

    Playing with the format object response

--- players.js
+++ players.js
@@ -20,12 +20,41 @@ player.get('/', function(req, res){
 	});
 });
 
+// inspecting the response object
 player.post('/', function(req, res){
 	console.log('(Using Router)  post');
 	backURL = req.header('Referer') || '/';
 	console.log(backURL);
 	player_names.push(req.body.name);
-	res.redirect(backURL);
+	//res.status(200);
+	//res.redirect(backURL);
+	//res.json(player_names);
+	res.jsonp(200, player_names);
 });
+
+// inspecting the request object
+player.get('/request-info', function(req, res){
+	console.log(req.param('attr')); //req.params.attr, req.body.attr, req.query.attr
+	console.log(req.originalUrl);
+	res.render('players.jade', {
+		title: 'Player',
+		names: player_names
+	});
+})
+
+// playing with the format response object
+player.get('/format', function(req, res){
+	res.format({
+		'text/plain': function(){
+			res.send('text/plain Response!');
+		},
+		'text/html': function(){
+			res.render('players_test.jade');
+		},
+		'application/json': function(){
+			res.json({json_obj: 'Testing format!'});
+		}
+	});
+})
 
 module.exports = player;
